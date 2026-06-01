49322c01cf255a23edc05c3fbc60214c84214b90
Bruno Oliveira <oliveiraalcantarab@gmail.com>
2026-06-01 12:14:07 -0300

    Fiz um monte de coisa

---
+++ djangoapp/blog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    return render(
+        request, 'blog/pages/index.html',
+        {
+            'nome': 'Bruno Oliveira'
+        }
+
+    )
--- djangoapp/project/settings.py
+++ djangoapp/project/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
+    'site_setup',
 ]
 
 MIDDLEWARE = [
@@ -65,6 +67,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'site_setup.context_processors.context_processor_example',
+                'site_setup.context_processors.site_setup',
             ],
         },
     },
