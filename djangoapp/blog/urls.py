a3de598860676fad325d27ca9b80b3059cd91c99
Bruno Oliveira <oliveiraalcantarab@gmail.com>
2026-06-01 14:25:58 -0300

    Adicionei as paginas page, post e alterei o css

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,9 +1,10 @@
-
+from blog.views import index, post, page
 from django.urls import path
-from blog.views import index
 
 
 app_name = 'blog'
 urlpatterns = [
     path('', index, name='index'),
+    path('post/', post, name='post'),
+    path('page/', page, name='page'),
 ]
--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -6,8 +6,16 @@ from django.shortcuts import render
 def index(request):
     return render(
         request, 'blog/pages/index.html',
-        {
-            'nome': 'Bruno Oliveira'
-        }
+    )
 
+
+def page(request):
+    return render(
+        request, 'blog/pages/page.html',
+    )
+
+
+def post(request):
+    return render(
+        request, 'blog/pages/post.html',
     )
