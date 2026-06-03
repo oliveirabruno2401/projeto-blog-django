8672a3f622ea75a1ebf88394d143c9537f5d4896
Bruno Oliveira <oliveiraalcantarab@gmail.com>
2026-06-03 11:22:00 -0300

    Iniciando a parte interna do Post

--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -1,8 +1,11 @@
+
+
 from django.db import models
 from utils.rands import slugify_new
 from django.contrib.auth.models import User
 from utils.images import resize_image
 from django_summernote.models import AbstractAttachment
+from django.urls import reverse
 
 
 class PostAttachment(AbstractAttachment):
@@ -85,10 +88,17 @@ class Page(models.Model):
         return self.title
 
 
+class PostManager(models.Manager):
+    def get_published(self):
+        return self.filter(is_published=True).order_by('-pk')
+
+
 class Post(models.Model):
     class Meta:
         verbose_name = 'Post'
         verbose_name_plural = 'Posts'
+
+    objects = PostManager()
 
     title = models.CharField(max_length=65,)
     slug = models.SlugField(
@@ -133,6 +143,11 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        if not self.is_published:
+            return reverse('blog:index')
+        return reverse('blog:post', args=(self.slug,))
 
     def save(self, *args, **kwargs):
         if not self.slug:
--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,3 +1,5 @@
+from venv import create
+
 from django.core.paginator import Paginator
 from django.shortcuts import render
 from blog.models import Post
@@ -6,11 +8,7 @@ PER_PAGE = 9
 
 
 def index(request):
-    posts = (Post.
-             objects.
-             filter(is_published=True).
-             order_by('-pk')
-             )
+    posts = Post.objects.get_published()
     paginator = Paginator(posts, PER_PAGE)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
@@ -24,7 +22,37 @@ def index(request):
     )
 
 
-def page(request):
+def created_by(request, author_pk):
+    posts = Post.objects.get_published().filter(created_by__pk=author_pk)
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
+    )
+
+
+def category(request, slug):
+    posts = Post.objects.get_published().filter(category__slug=slug)
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
+    )
+
+
+def page(request, slug):
     return render(
         request,
         'blog/pages/page.html',
@@ -34,11 +62,13 @@ def page(request):
     )
 
 
-def post(request):
+def post(request, slug):
+    post = (Post.objects.get_published().filter(slug=slug).first())
+
     return render(
         request,
         'blog/pages/post.html',
         {
-            # 'page_obj': page_obj,
+            'post': post,
         }
     )
